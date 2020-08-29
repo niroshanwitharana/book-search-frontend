7f9a7b7febd3c9cd2d264536fc73c358d749e031
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-29 10:17:33 +0100

    creating a repo for front-end

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>©2020 -Niro-</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
