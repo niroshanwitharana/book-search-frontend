d61e90734c0bd1823022a3f353217ecfe6f1d8d6
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-09-28 00:36:03 +0100

    updated API routes

--- src/utils/API.js
+++ src/utils/API.js
@@ -8,22 +8,22 @@ export default {
 
     // Gets all books
     getBooks: function () {
-        return axios.get("/api/books");
+        return axios.get("https://niro-book-search-backend.herokuapp.com/api/books");
     },
 
     // Gets the book with the given id
     getBook: function (id) {
-        return axios.get("/api/books/" + id);
+        return axios.get("https://niro-book-search-backend.herokuapp.com/api/books/" + id);
     },
 
     // Saves a book to the database
     saveBook: function (savedBooks) {
-        return axios.post("/api/books", savedBooks);
+        return axios.post("https://niro-book-search-backend.herokuapp.com/api/books", savedBooks);
     },
 
     // Deletes the book with the given id
     deleteBook: function (id) {
-        return axios.delete("/api/books/" + id);
+        return axios.delete("https://niro-book-search-backend.herokuapp.com/api/books/" + id);
     }
 
 }
